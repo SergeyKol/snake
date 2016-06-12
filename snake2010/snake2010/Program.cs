007942e4b74e72e4855d2da15760ce9e07d7b6a9
SergeyKol <koseva@yandex.ru>
2016-06-12 23:56:39 +0400

    Конструкторы и Инкапсуляция

--- snake2010/snake2010/Program.cs
+++ snake2010/snake2010/Program.cs
@@ -10,11 +10,11 @@ namespace snake2010
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
+            Point p1 = new Point(1, 3, '*');
+            /*p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-
+            */
             p1.Draw();
 
             Point p2 = new Point();
