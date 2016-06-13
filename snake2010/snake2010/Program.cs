dd25b5ee95611df65efaeaddb93ecbae8cbc7e27
SergeyKol <koseva@yandex.ru>
2016-06-13 16:59:48 +0400

    новый класс HorizontalLine

--- snake2010/snake2010/Program.cs
+++ snake2010/snake2010/Program.cs
@@ -16,44 +16,8 @@ namespace snake2010
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-
-            List<char> charList = new List<char>();
-            charList.Add('*');
-            charList.Add('#');
-            charList.Add('$');
-
-            char a = charList[0];
-            char b = charList[1];
-            char c = charList[2];
-
-            foreach (char j in charList)
-            {
-                Console.WriteLine(j);
-            }
-
-            charList.RemoveAt(0);
-
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
+            HorizontalLine Line = new HorizontalLine(5,10,8,'+');
+            Line.Drow();
 
             Console.ReadLine();
         }
